709acc36adf181b2bd5f4adb8ad3d35a4d41e6c7
Ivan Botero <ivan.botero@protonmail.ch>
2017-05-14 19:26:14 -0400

    Imprimimos nuestro primer mensaje en pantall

--- kmain.c
+++ kmain.c
@@ -1,10 +1,38 @@
+void cls() {
+
+    char *text_video = (char*) 0x000B8000;
+    int i;
+
+    for (i = 0; i < 4000; i++) {
+        *text_video++ = 0x00;
+    }
+
+    return;
+}
+
 int kmain(void) {
 
+    cls();
 
     char *fb = (char *) 0x000B8000;
 
-    fb[0] = 'A';
-    fb[1] = 0x28;
+    *fb++ = 'c';
+    *fb++ = 0x07;
+
+    *fb++ = 'a';
+    *fb++ = 0x07;
+
+    *fb++ = 'f';
+    *fb++ = 0x07;
+
+    *fb++ = 'e';
+    *fb++ = 0x07;
+
+    *fb++ = 'O';
+    *fb++ = 0x07;
+
+    *fb++ = 'S';
+    *fb++ = 0x07;
 
     return 0;
 }
