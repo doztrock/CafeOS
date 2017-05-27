0bf3b7ac3179dccca9189fdddd1731dbc742f6db
Ivan Botero <ivan.botero@protonmail.ch>
2017-05-27 12:21:31 -0500

    Implementacion inicial de teclado

---
+++ lib/kernel/teclado.h
@@ -0,0 +1,15 @@
+/**
+ * Nombre:    IO.H
+ * 
+ * Contenido: Constantes respectivas al codigo ASCII, 
+ *            para permitir un reconocimiento basico de los caracteres del teclado.
+ * 
+ */
+
+#ifndef TECLADO_H
+#define TECLADO_H
+
+#include "teclado/mapa.h"
+
+
+#endif /* TECLADO_H */
