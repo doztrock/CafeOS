833f954d7ac8a652335f514ca40bd507b068f7cd
Ivan Botero <ivan.botero@protonmail.ch>
2017-06-16 11:18:28 -0500

    Agregada funcion de escritura a dispositivo

--- lib/kernel/dispositivo/dispositivo.h
+++ lib/kernel/dispositivo/dispositivo.h
@@ -16,10 +16,16 @@
 #define CANTIDAD_MAXIMA_DISPOSITIVOS    64
 
 /**
+ * Prototipo de funcion para escritura en dispositivo
+ */
+typedef void (* _escritura) (unsigned char *, unsigned short);
+
+/**
  * Estructura del dispositivo
  */
 struct dispositivo {
     uint16_t identificador;
+    _escritura escritura;
 } __attribute__ ((packed));
 
 /**
@@ -32,6 +38,6 @@ struct dispositivo TABLA_DISPOSITIVOS[CANTIDAD_MAXIMA_DISPOSITIVOS];
  * Funciones
  */
 void registrarDispositivo(struct dispositivo dispositivo);
-
+void escrituraDispositivo(uint16_t identificador, unsigned char *buffer, uint16_t n);
 
 #endif /* DISPOSITIVO_H */
