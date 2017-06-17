8063081b8e1a801cc3a58a28dc11b1948b14d867
Ivan Botero <ivan.botero@protonmail.ch>
2017-06-17 11:35:50 -0500

    Agregado funcionamiento basico para Puerto Serial -> Por el momento solo habilitado para COM1

--- dispositivos/serial.c
+++ dispositivos/serial.c
@@ -1,1 +1,104 @@
 #include "serial.h"
+
+/**
+ * Registro del dispositivo
+ */
+static struct dispositivo DISPOSITIVO_SERIAL = {
+    .identificador = 0,
+    .escritura = escrituraSerial
+};
+
+/**
+ * Funcion:  inicializarSerial
+ * 
+ * Objetivo: Registrar un dispositivo en la tabla de dispositivos.
+ * 
+ * @param informacion Estructura con la informacion del dispositivo.
+ * @return            No tiene ningun valor de retorno.
+ */
+void inicializarSerial(void) {
+
+    /* Puerto COM */
+    struct puertoCOM COM1;
+
+    /* Configuramos el Puerto COM */
+    configurarPuertoSerial(COM1);
+
+    /* Registramos el dispositivo */
+    registrarDispositivo(DISPOSITIVO_SERIAL);
+
+    return;
+}
+
+/**
+ * Funcion:  escrituraSerial
+ * 
+ * Objetivo: Registrar un dispositivo en la tabla de dispositivos.
+ * 
+ * @param informacion Estructura con la informacion del dispositivo.
+ * @return            No tiene ningun valor de retorno.
+ */
+void escrituraSerial(unsigned char *buffer, unsigned short n) {
+
+    uint32_t i = 0;
+
+    /* Verificamos si la escritura hacia el puerto esta ocupada */
+    while (escrituraSerialOcupada(PUERTO_COM1) == 0);
+
+    while (i < n) {
+        outb(PUERTO_COM1, buffer[i++]);
+    }
+
+    return;
+}
+
+char lecturaSerial(uint16_t com) {
+
+    /* Verificamos si la lectura del el puerto esta ocupada */
+    while (lecturaSerialOcupada(com) == 0);
+
+    return (char) inb(com);
+}
+
+void configurarVelocidadSerial(struct puertoCOM puerto) {
+
+    outb(SERIAL_PUERTO_LINEA(puerto.com), SERIAL_HABILITAR_DLAB);
+    outb(SERIAL_PUERTO_DATOS(puerto.com), (puerto.divisor >> 8) & 0x00FF);
+    outb(SERIAL_PUERTO_DATOS(puerto.com), puerto.divisor & 0x00FF);
+    outb(SERIAL_PUERTO_LINEA(puerto.com), 0x00);
+
+    return;
+}
+
+void configurarLineaSerial(struct puertoCOM puerto) {
+    outb(SERIAL_PUERTO_LINEA(puerto.com), 0x03);
+    return;
+}
+
+void configurarBufferSerial(struct puertoCOM puerto) {
+    outb(SERIAL_PUERTO_FIFO(puerto.com), 0xC7);
+    return;
+}
+
+void configurarModemSerial(struct puertoCOM puerto) {
+    outb(SERIAL_PUERTO_MODEM(puerto.com), 0x03);
+    return;
+}
+
+void configurarPuertoSerial(const struct puertoCOM puerto) {
+
+    configurarVelocidadSerial(puerto);
+    configurarLineaSerial(puerto);
+    configurarBufferSerial(puerto);
+    configurarModemSerial(puerto);
+
+    return;
+}
+
+uint32_t escrituraSerialOcupada(uint16_t com) {
+    return inb(SERIAL_ESTADO_PUERTO_LINEA(com)) & 0x20;
+}
+
+uint32_t lecturaSerialOcupada(uint16_t com) {
+    return inb(SERIAL_ESTADO_PUERTO_LINEA(com)) & 1;
+}
--- dispositivos/serial.h
+++ dispositivos/serial.h
@@ -8,4 +8,86 @@
 #ifndef SERIAL_H
 #define SERIAL_H
 
+#include "kernel/io.h"
+#include "kernel/dispositivo/dispositivo.h"
+
+#include "stdint.h"
+
+/**
+ * Puertos COM
+ */
+#define PUERTO_COM1 0x3F8
+#define PUERTO_COM2 0x2F8
+#define PUERTO_COM3 0x3E8
+#define PUERTO_COM4 0x2E8
+
+/**
+ * Estado de puertos de cada Puerto COM
+ * 
+ * De acuerdo a la documentacion:
+ * 
+ * Register map for a given COM port:
+ *
+ *	+========+======================================================+
+ *	| Offset | Description						|
+ *	+========+======================================================+
+ *	| +0     | Data register. If DLAB is disabled, reading from     |
+ *	|        | this register is reading from the RECEIVE buffer and |
+ *	|        | writing to it is the same as writing to the TRANSMIT |
+ *	|        | buffer. If DLAB is enabled then this will hold the   |
+ *	|        | low byte of the baud rate divisor value.             |
+ *	+--------+------------------------------------------------------+
+ *	| +1     | Interrupt enabling register. If DLAB is enabled,     |
+ *	|        | this register will hold the high byte of the baud    |
+ *	|        | rate divisor value.                                  |
+ *	+--------+------------------------------------------------------+
+ *	| +2     | Interrupt identification and buffer / FIFO config.   |
+ *	+--------+------------------------------------------------------+
+ *	| +3     | Line control register. Configures the line protocol. |
+ *	+--------+------------------------------------------------------+
+ *	| +4     | Modem control register.                              |
+ *	+--------+------------------------------------------------------+
+ *	| +5     | Line status register.                                |
+ *	+--------+------------------------------------------------------+
+ *	| +6     | Modem status register.                               |
+ *	+--------+------------------------------------------------------+
+ *	| +7     | Scratch register.                                    |
+ *	+========+======================================================+
+ */
+#define SERIAL_PUERTO_DATOS(base)           (base)
+#define SERIAL_PUERTO_FIFO(base)            (base + 2)
+#define SERIAL_PUERTO_LINEA(base)           (base + 3)
+#define SERIAL_PUERTO_MODEM(base)           (base + 4)
+#define SERIAL_ESTADO_PUERTO_LINEA(base)    (base + 5)
+
+/**
+ * Constante de habilitacion del DLAB (Divisor Latch Access Bit)
+ */
+#define SERIAL_HABILITAR_DLAB             0x80
+
+/**
+ * Estructura del Puerto COM
+ */
+struct puertoCOM {
+    uint16_t com;
+    uint16_t divisor;
+};
+
+
+/**
+ * Funciones
+ */
+void inicializarSerial(void);
+void escrituraSerial(unsigned char *buffer, unsigned short n);
+char lecturaSerial(uint16_t com);
+
+void configurarVelocidadSerial(struct puertoCOM puerto);
+void configurarLineaSerial(struct puertoCOM puerto);
+void configurarBufferSerial(struct puertoCOM puerto);
+void configurarModemSerial(struct puertoCOM puerto);
+void configurarPuertoSerial(const struct puertoCOM puerto);
+
+uint32_t escrituraSerialOcupada(uint16_t com);
+uint32_t lecturaSerialOcupada(uint16_t com);
+
 #endif /* SERIAL_H */
